72bfdcb202fc3666679b3aabe1f477ea51d72905
haileyesus <teamdevs7@gmail.com>
2022-11-09 05:08:56 +0000

    added a title

--- destructuring.js
+++ destructuring.js
@@ -3,8 +3,47 @@
 //destructuring an array
 const arr =['hello','this','is','destructuring'];
 
-const [x,y,a,b] =arr;
+let [x,y,a,b] =arr;
 console.log(x,y,a,b);
+
+//switching the value by destructuring
+[x,y]=[y,x]
+console.log(x,y);
+
+//destructuring array from object
+const hotel = {
+    breakFast : ['pizza','burger'],
+    dinner :['juice','vegitabel'],
+    order: function(indexOne,indexTwo){
+        return [this.breakFast[indexOne], this.dinner[indexTwo]];
+    }
+}
+const [food] = hotel.breakFast;
+console.log(food);
+console.log(hotel.order(0,1));
+
+//destructuring
+const [starter,final] = hotel.order(0,1);
+console.log(starter,final);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
